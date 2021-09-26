9bd1e91ea3af6465de473a8dc1718acf81e64a9c
irakeshm <rakeshrenukut@gmail.com>
2021-09-26 06:54:27 +0530

    Added Resume

--- assets/js/main.js
+++ assets/js/main.js
@@ -101,17 +101,17 @@ let resumeButton = document.getElementById('resume-button')
 
 
 // Function to call areaCv and Html2Pdf options 
-let opt = {
-    margin: 0,
-    filename: 'Rakesh_Resume.pdf',
-    image: { type: 'jpeg', quality: 0.98 },
-    html2canvas: { scale: 4 },
-    jsPDF: { format: 'a4', orientation: 'portrait' }
-};
-function generateResume() {
-    html2pdf(areaCv, opt)
+// let opt = {
+//     margin: 0,
+//     filename: 'Rakesh_Resume.pdf',
+//     image: { type: 'jpeg', quality: 0.98 },
+//     html2canvas: { scale: 4 },
+//     jsPDF: { format: 'a3', orientation: 'portrait' }
+// };
+// function generateResume() {
+//     html2pdf(areaCv, opt)
 
-}
+// }
 
 // When the button is clicked, it executes the three functions
 
@@ -124,6 +124,6 @@ function generateResume() {
 // 3. The .scale-cv class is removed from the body after 5 seconds to return to normal size.
 resumeButton.addEventListener('click', () => {
     scalecv()
-    generateResume()
+    //generateResume()
     setTimeout(removeScale, 5000)
 })
